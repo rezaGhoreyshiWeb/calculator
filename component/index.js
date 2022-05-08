a6571bf41c870646a53d3c3a306b1f862caf4d1a
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2022-05-08 16:23:00 +0430

    add the actionsChecker

--- component/index.js
+++ component/index.js
@@ -1,5 +1,6 @@
 import { switcherTheme } from "./switcherTheme.js";
 import { onloadThemeChecker } from "./onloadThemeChecker.js";
+import { actionsChecker } from "./actionsChecker.js";
 // elements
 const switcherEl = document.getElementById("switcher");
 const actionsEl = document.querySelectorAll(".actions");
@@ -7,4 +8,9 @@ const actionsEl = document.querySelectorAll(".actions");
 // events
 switcherEl.addEventListener("click", switcherTheme);
 window.addEventListener("load", onloadThemeChecker);
-actionsEl.forEach(el => el.addEventListener("click", (e) => console.log(e.target.dataset.value)))
+actionsEl.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    const value = e.target.dataset.value ?? e.target.textContent.trim();
+    actionsChecker(value);
+  })
+);
