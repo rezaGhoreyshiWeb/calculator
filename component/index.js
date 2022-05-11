0fe3925eef2fd9c1fa7bbdafc9deced266485ca3
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2022-05-11 13:32:43 +0430

    move the some of the functions to index.js and change the remider with remove icon

--- component/index.js
+++ component/index.js
@@ -1,9 +1,15 @@
 import { switcherTheme } from "./switcherTheme.js";
 import { onloadThemeChecker } from "./onloadThemeChecker.js";
-import { actionsChecker } from "./actionsChecker.js";
 // elements
 const switcherEl = document.getElementById("switcher");
 const actionsEl = document.querySelectorAll(".actions");
+const calculatesEl = document.getElementById("calculates");
+const clearEl = document.getElementById("clear");
+
+// declared variables
+let bracket;
+let num;
+let actionsArray = [];
 
 // events
 switcherEl.addEventListener("click", switcherTheme);
@@ -11,6 +17,18 @@ window.addEventListener("load", onloadThemeChecker);
 actionsEl.forEach((el) =>
   el.addEventListener("click", (e) => {
     const value = e.target.dataset.value ?? e.target.textContent.trim();
+
     actionsChecker(value);
   })
 );
+
+// functions
+
+function actionsChecker(value) {
+  showActions();
+}
+
+function showActions() {
+  calculatesEl.classList.remove("hidden");
+  calculatesEl.innerHTML = "";
+}
